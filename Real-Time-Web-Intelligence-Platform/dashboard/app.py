cb1103045221216274472ddf70cdd73e9d823c16
aasthika <aasthika@aasthikas-MacBook-Air.local>
2026-04-03 10:32:08 +0530

    finished Real Time Web Intelligence Microservices

--- Real-Time-Web-Intelligence-Platform/dashboard/app.py
+++ Real-Time-Web-Intelligence-Platform/dashboard/app.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import time
 from streamlit_autorefresh import st_autorefresh
-API_URL = "https://web-intelligence-api.onrender.com"
+API_URL = "http://api:8000"
 
 st.set_page_config(
     page_title="Real Time Web Intelligence",
